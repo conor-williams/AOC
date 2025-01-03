8ea3fea60ca7a6c89ec2217c6f32a9e7e8f1c1da
conor-williams <conor.williams@gmail.com>
2025-01-03 23:16:29 +0000

    slight bug in day1.c + tidy

--- ALL.2017/day1.2.c
+++ ALL.2017/day1.2.c
@@ -4,17 +4,39 @@
 #include <ctype.h>
 #include <math.h>
 
+#include <unistd.h>
+
+#define getchar()
 int pos = 0;
 int main(int argc, char **argv)
 {
         printf("%d", argc); printf("%s", argv[1]); fflush(stdin); fflush(stdout);
 
-        FILE * a = fopen(argv[1], "r"); printf("Day8.2\n"); fflush(stdin); fflush(stdout);
+        FILE * a = fopen(argv[1], "r"); printf("2017 Day1.2\n"); fflush(stdin); fflush(stdout);
 
-        char line1[3000];
-        fgets(line1, 3000, a);
+	fflush(stdout); int fd = dup(1); close(1);
+
+        char line1[4000];
+        char line2[5000];
+        char line3[4500];
+        fgets(line1, 3000-1, a);
         fclose(a);
+	line1[(int)strlen(line1)-1] = '\0';
 printf("line1 %s\n", line1);
+	int kk;
+	for (kk = 0; kk < (int)strlen(line1); kk++) {
+		if (line1[kk] == line1[(int)strlen(line1) -1]) {
+			//good
+		} else {
+			break;
+		}
+	}
+	strcpy(line2, line1);
+	for (int zz = 0; zz < kk; zz++) {
+		strcpy(line3, line2);
+		sprintf(line2, "%s%c", line3, line1[(int)strlen(line1)-1]);
+	}
+	strcpy(line1, line2);
 
 int tot = 0;
         int bprev = line1[0] - '0';
@@ -28,6 +50,8 @@ int tot = 0;
 	     }
              bprev = line1[f-forward+1] - '0';
         }
+
+	dup2(fd, 1);
         printf("******TOT: %d\n", tot*2);
 }
 
--- ALL.2017/day1.c
+++ ALL.2017/day1.c
@@ -4,17 +4,41 @@
 #include <ctype.h>
 #include <math.h>
 
+#include <unistd.h>
+
+#define getchar()
 int pos = 0;
 int main(int argc, char **argv)
 {
         printf("%d", argc); printf("%s", argv[1]); fflush(stdin); fflush(stdout);
 
-        FILE * a = fopen(argv[1], "r"); printf("Day8.2\n"); fflush(stdin); fflush(stdout);
+        FILE * a = fopen(argv[1], "r"); printf("2017 Day1.1\n"); fflush(stdin); fflush(stdout);
 
-        char line1[3000];
+	fflush(stdout); int fd = dup(1); close(1);
+
+        char line1[4000];
+        char line2[5000];
+        char line3[4500];
         fgets(line1, 3000, a);
         fclose(a);
 printf("line1 %s\n", line1);
+
+	line1[(int)strlen(line1)-1] = '\0';
+printf("line1 %s\n", line1);
+	int kk;
+	for (kk = 0; kk < (int)strlen(line1); kk++) {
+		if (line1[kk] == line1[(int)strlen(line1) -1]) {
+			//good
+		} else {
+			break;
+		}
+	}
+	strcpy(line2, line1);
+	for (int zz = 0; zz < kk; zz++) {
+		strcpy(line3, line2);
+		sprintf(line2, "%s%c", line3, line1[(int)strlen(line1)-1]);
+	}
+	strcpy(line1, line2);
 
 int tot = 0;
         int bprev = line1[0] - '0';
@@ -27,6 +51,8 @@ int tot = 0;
 	     }
              bprev = bNum;
         }
+
+	dup2(fd, 1);
         printf("******TOT: %d\n", tot);
 }
 
