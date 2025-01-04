462ecc67968fbef176d630963e47d5aa2deebda3
conor-williams <conor.williams@gmail.com>
2025-01-04 20:07:33 +0000

    improved + clearer

--- ALL.2018/day10.2.c
+++ ALL.2018/day10.2.c
@@ -34,7 +34,7 @@ struct pos posit[500] = {0};
 int main(int argc, char **argv)
 {
 	printf("%d", argc); printf("@%s", argv[1]); fflush(stdout);
-	FILE * a = fopen(argv[1], "r"); printf("2018 Day10.1\n"); fflush(stdout);
+	FILE * a = fopen(argv[1], "r"); printf("2018 Day10.2\n"); fflush(stdout);
 
 	int leny = 0;
 	while (1)
@@ -72,7 +72,6 @@ int main(int argc, char **argv)
 
 //	for (int i = 10887; i < UP; i++) {
 	map <int, int> countx;
-	int boardCon;
 	for (int i = 0; i < UP; i++) {
 		countx.clear();
 		//printf("AGAINTOP? %d:", i);
@@ -84,9 +83,8 @@ int main(int argc, char **argv)
 			//if (posit[z].xs[i] < 0) {printf("too small.. \n"); fflush(stdout);exit(0);}
 			//countx[posit[z].xs[i]]++;
 			if (countx[posit[z].ys[i]] > 40) {
-				boardCon = i;
-				printf("boardCon is %d\n", boardCon);
-				goto after;
+				printf("ans: %d\n", i);
+				exit(0);
 			}
 			///printf("\x1b[%dB", posit[z].ys[i]); //printf("%d", posit[z].ys[i]); getchar();
 			///printf("\x1b[%dC#", posit[z].xs[i]);
@@ -98,28 +96,7 @@ int main(int argc, char **argv)
 		//getchar();
 		//printf("\x1b[2J");
 	}
-after:
-	printf("after...\n");
-	int minX = 999999999;
-	int maxX = 0;
-	int minY = 999999999;
-	int maxY = 0;
-	for (int z = 0; z < leny; z++) {
-		if (posit[z].xs[boardCon] > maxX) {maxX = posit[z].xs[boardCon];}
-		if (posit[z].xs[boardCon] < minX) {minX = posit[z].xs[boardCon];}
-		if (posit[z].ys[boardCon] > maxY) {maxY = posit[z].ys[boardCon];}
-		if (posit[z].ys[boardCon] < minY) {minY = posit[z].ys[boardCon];}
-	}
-	printf("%d,%d -> %d,%d\n", minX, minY, maxX, maxY);
-	printf("\x1b[2J");
-	for (int z = 0; z < leny; z++) {
-		printf("\x1b[H");
-		printf("\x1b[%dB", posit[z].ys[boardCon]-minY);
-		printf("\x1b[%dC#", posit[z].xs[boardCon]-minX);
-		//printf("%d\n", posit[z].ys[boardCon]);
-		//printf("%d\n", posit[z].xs[boardCon]);
-	}
-	printf("\n\n\n");
-	//printf("**tot %lu\n", tot);
+
+	printf("**tot %lu\n", tot);
 }
 			
