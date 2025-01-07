d2298571f1582b1d175f9d14b2ec7f91b80ed741
conor-williams <conor.williams@gmail.com>
2025-01-07 21:26:34 +0000

    day5 ok (day5.2 needs optim)

--- ALL.2023/day5.2.c
+++ ALL.2023/day5.2.c
@@ -3,14 +3,24 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <map>
 
+using namespace std;
+
+#include <unistd.h>
+
+map <unsigned long long, unsigned long long> mp[10];
+unsigned long long getLocation(unsigned long long in);
+unsigned long long lookup(unsigned long long in, int mppos);
+
+#define getchar()
 int isAlready(int num);
 int arr[10000] = {0};
 int endarr = 0;
 struct mymap {
-    long to;
-    long from;
-    long range;
+	unsigned long long from;
+	unsigned long long to;
+	unsigned long long range;
 };
 struct mymap stos[400];
 struct mymap stof[400];
@@ -21,98 +31,114 @@ struct mymap ttoh[400];
 struct mymap htol[400];
 
 struct mymap *pmap[7];
-void main(int argc, char **argv)
+int pmapNum[10];
+int main(int argc, char **argv)
 {
-        printf("%d", argc);
-        printf("%s", argv[1]);
-        fflush(stdin); fflush(stdout);
-pmap[0] = stos;
-pmap[1] = stof;
-pmap[2] = ftow;
-pmap[3] = wtol;
-pmap[4] = ltot;
-pmap[5] = ttoh;
-pmap[6] = htol;
+	printf("SLOW ~15 mins\n");
+	printf("%d", argc);
+	printf("%s", argv[1]);
+	fflush(stdin); fflush(stdout);
+	pmap[0] = stos;
+	pmap[1] = stof;
+	pmap[2] = ftow;
+	pmap[3] = wtol;
+	pmap[4] = ltot;
+	pmap[5] = ttoh;
+	pmap[6] = htol;
 
-        FILE * a = fopen(argv[1], "r");
-        printf("Day5.1\n");
-        fflush(stdin); fflush(stdout);
-        char line1[1000];
-        int linenum = 0;
+	FILE * a = fopen(argv[1], "r");
+	printf("2023 Day5.2\n");
+	fflush(stdin); fflush(stdout);
 
-int poswithin = 0;
-        fgets(line1, 1000, a); //seeds
-        fgets(line1, 1000, a); //blank
-int mymappos = -1;
-//POPULATE... Great...
-        while (1) {
-            fgets(line1, 1000, a);
-	    if (strlen(line1) == 1) {fgets(line1, 1000, a);}
-	    if (strstr(line1, "map")) {
-		fgets(line1, 1000, a); mymappos++; poswithin=0;
+	fflush(stdout); int fd = dup(1); close(1);
+	char line1[1000];
+	int linenum = 0;
 
-
-}
-	    //printf("%s", line1);
-            if (feof(a)) break;
-            linenum++;
+	int poswithin = 0;
+	fgets(line1, 1000, a); //seeds
+	char seeds123[1000];
+	line1[(int)strlen(line1)-1] = '\0';
+	strcpy(seeds123, line1);
+	fgets(line1, 1000, a); //blank
+	int mymappos = -1;
+	//POPULATE... Great...
+	memset(pmapNum, 0, sizeof(pmapNum));
+	while (1) {
+		fgets(line1, 1000, a);
+		if (strlen(line1) == 1) {fgets(line1, 1000, a);}
+		if (strstr(line1, "map")) {
+			fgets(line1, 1000, a); mymappos++; poswithin=0;
+		}
+		printf("%s", line1);
+		if (feof(a)) break;
+		linenum++;
 		pmap[mymappos][poswithin].to = 0;
 		pmap[mymappos][poswithin].from = 0;
 		pmap[mymappos][poswithin].range = 0;
-	    sscanf(line1, "%d %d %d",
-		&pmap[mymappos][poswithin].to,
-		&pmap[mymappos][poswithin].from,
-		&pmap[mymappos][poswithin].range);
-	    //printf("got: %ld %ld %ld\n", pmap[mymappos][poswithin].to, pmap[mymappos][poswithin].from, pmap[mymappos][poswithin].range);
-	    poswithin++;
-        }
-//printf("RAND: %ld\n", pmap[6][3].range);
-
-long NUM = 0;;
-//long NUMinput[] = {2880930400, 17599561, 549922357, 200746426, 1378552684, 43534336, 155057073, 56546377, 824205101, 378503603, 1678376802, 130912435, 2685513694, 137778160, 2492361384, 188575752, 3139914842, 1092214826, 2989476473, 58874625, 0};
-long NUMinput[] = {2880930400, 549922357, 1378552684, 155057073, 824205101, 1678376802, 2685513694, 2492361384, 3139914842, 2989476473, 0};
-long NUMrange[] = {17599561, 200746426, 43534336, 56546377, 378503603, 130912435, 137778160, 188575752, 1092214826, 58874625, 0};
-//long NUMinput[] = {79, 55, 0};
-//long NUMrange[] = {14, 13, 0};
-
-int m = 0;
-long NUMsmallest = 0;
-        
-    for (m = 0; NUMinput[m] != 0; m++) {
-       long n = 0;
-
-       while (n < NUMrange[m]) {
-        NUM = NUMinput[m] + n;
-if (m == 0 && n == 0) {NUMsmallest = NUM;}
-//printf("NUM %ld\n", NUM);
-
-	for (int k = 0; k < 7; k++) {
-	    int l=0;
-            while (1) {
-		if (pmap[k][l].to == 0 && pmap[k][l].from == 0 &&
-		    pmap[k][l].range == 0) { break;}
-		if (pmap[k][l].from <= NUM) {
-		     if (pmap[k][l].from + pmap[k][l].range >= NUM) {
-			NUM = NUM - pmap[k][l].from + pmap[k][l].to;
-			break;
-		     }
-		}
-                l++;
-	    }
+		sscanf(line1, "%llu %llu %llu",
+				&pmap[mymappos][poswithin].to,
+				&pmap[mymappos][poswithin].from,
+				&pmap[mymappos][poswithin].range);
+		//printf("got: %llu %llu %llu\n", pmap[mymappos][poswithin].to, pmap[mymappos][poswithin].from, pmap[mymappos][poswithin].range);
+		pmapNum[mymappos]++;
+		poswithin++;
 	}
-	//printf("Location[%d]: %ld\n", m, NUM);
-        
-        if (NUMsmallest > NUM) {NUMsmallest = NUM;}
 
-       n++;
-       }
-    }
-//find smallest
-	printf("Last Location: %ld\n", NUM);
-	printf("Location Smallest: %ld\n", NUMsmallest);
-        fflush(stdin); fflush(stdout);
-        fclose(a);
-//printf("*** Score:  %d ****\n", score);
-printf("%d", linenum);
+
+	char * fir = strtok(seeds123, ":");
+	fir = strtok(NULL, ":");
+	char nuIn[1000];
+	strcpy(nuIn, fir);
+	char *ne = strtok(nuIn, " \0");
+	int pos123 = 0;
+	unsigned long long NUMinput[50];
+	unsigned long long NUMrange[50];
+	do {
+		char *bla;
+		if (pos123 % 2 == 0) {
+			NUMinput[pos123/2] = strtoull(ne, &bla, 10);
+		} else {
+			NUMrange[pos123/2] = strtoull(ne, &bla, 10);
+		}
+		pos123++;
+	} while ((ne = strtok(NULL, " \0")) != NULL);
+	NUMinput[(pos123/2)] = 0;
+	NUMrange[(pos123/2)] = 0;
+
+	int m = 0;
+	////////////
+
+	unsigned long long NUMsmall = 99999999999999;
+	unsigned long long NUM = 0;;
+	for (m = 0; NUMinput[m] != 0; m++) {
+		printf("m is %d\n", m);
+		for (unsigned long long kkk = 0; kkk < NUMrange[m]; kkk++) {
+			NUM = NUMinput[m]+kkk;
+			unsigned long long retNUM1 = getLocation(NUM);
+			if (retNUM1 < NUMsmall) {NUMsmall = retNUM1;}
+		}
+	}
+
+	fflush(stdout); dup2(fd, 1);
+	printf("**ans: %llu\n", NUMsmall);
 }
-
+unsigned long long getLocation(unsigned long long in) {
+	unsigned long long tmp1 = lookup(in, 0);
+	unsigned long long tmp2 = lookup(tmp1, 1);
+	unsigned long long tmp3 = lookup(tmp2, 2);
+	unsigned long long tmp4 = lookup(tmp3, 3);
+	unsigned long long tmp5 = lookup(tmp4, 4);
+	unsigned long long tmp6 = lookup(tmp5, 5);
+	unsigned long long tmp7 = lookup(tmp6, 6);
+	return tmp7;
+}
+unsigned long long lookup(unsigned long long in, int mppos) {
+	for (int i = 0; i < pmapNum[mppos]; i++) {
+		if (in >= pmap[mppos][i].from  && in <= pmap[mppos][i].from+pmap[mppos][i].range) {
+			return pmap[mppos][i].to + in - pmap[mppos][i].from;
+		} else {
+			continue;
+		}
+	}
+	return in;
+}
--- ALL.2023/day5.c
+++ ALL.2023/day5.c
@@ -3,17 +3,19 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
-
 #include <unistd.h>
+
+unsigned long long getLocation(unsigned long long in);
+unsigned long long lookup(unsigned long long in, int mppos);
 
 #define getchar()
 int isAlready(int num);
 int arr[10000] = {0};
 int endarr = 0;
 struct mymap {
-	long to;
-	long from;
-	long range;
+	unsigned long long from;
+	unsigned long long to;
+	unsigned long long range;
 };
 struct mymap stos[400];
 struct mymap stof[400];
@@ -24,6 +26,7 @@ struct mymap ttoh[400];
 struct mymap htol[400];
 
 struct mymap *pmap[7];
+int pmapNum[10];
 int main(int argc, char **argv)
 {
 	printf("%d", argc);
@@ -38,7 +41,8 @@ int main(int argc, char **argv)
 	pmap[6] = htol;
 
 	FILE * a = fopen(argv[1], "r");
-	printf("2023 Day5.1\n"); fflush(stdin); fflush(stdout);
+	printf("2023 Day5.1\n");
+	fflush(stdin); fflush(stdout);
 
 	fflush(stdout); int fd = dup(1); close(1);
 	char line1[1000];
@@ -46,16 +50,18 @@ int main(int argc, char **argv)
 
 	int poswithin = 0;
 	fgets(line1, 1000, a); //seeds
+	char seeds123[1000];
+	line1[(int)strlen(line1)-1] = '\0';
+	strcpy(seeds123, line1);
 	fgets(line1, 1000, a); //blank
 	int mymappos = -1;
 	//POPULATE... Great...
+	memset(pmapNum, 0, sizeof(pmapNum));
 	while (1) {
 		fgets(line1, 1000, a);
 		if (strlen(line1) == 1) {fgets(line1, 1000, a);}
 		if (strstr(line1, "map")) {
 			fgets(line1, 1000, a); mymappos++; poswithin=0;
-
-
 		}
 		printf("%s", line1);
 		if (feof(a)) break;
@@ -63,56 +69,62 @@ int main(int argc, char **argv)
 		pmap[mymappos][poswithin].to = 0;
 		pmap[mymappos][poswithin].from = 0;
 		pmap[mymappos][poswithin].range = 0;
-		sscanf(line1, "%ld %ld %ld",
+		sscanf(line1, "%llu %llu %llu",
 				&pmap[mymappos][poswithin].to,
 				&pmap[mymappos][poswithin].from,
 				&pmap[mymappos][poswithin].range);
-		printf("got: %ld %ld %ld\n", pmap[mymappos][poswithin].to, pmap[mymappos][poswithin].from, pmap[mymappos][poswithin].range);
+		//printf("got: %llu %llu %llu\n", pmap[mymappos][poswithin].to, pmap[mymappos][poswithin].from, pmap[mymappos][poswithin].range);
+		pmapNum[mymappos]++;
 		poswithin++;
 	}
-	printf("RAND: %ld\n", pmap[6][3].range);
 
-	long NUM;
-	long NUMinput[] = {2880930400, 17599561, 549922357, 200746426, 1378552684, 43534336, 155057073, 56546377, 824205101, 378503603, 1678376802, 130912435, 2685513694, 137778160, 2492361384, 188575752, 3139914842, 1092214826, 2989476473, 58874625, 0};
-	//long NUMinput[] = {79, 14, 55, 13, 0};
-	//TRACE 2880930400
-	//.from -> .to
-	//.to becomes .from for next etc...
-	//long NUM = 2880930400;
+
+	char * fir = strtok(seeds123, ":");
+	fir = strtok(NULL, ":");
+	char nuIn[1000];
+	strcpy(nuIn, fir);
+	char *ne = strtok(nuIn, " \0");
+	int pos123 = 0;
+	unsigned long long NUMboth[50];
+	do {
+		char *bla;
+		NUMboth[pos123] = strtoull(ne, &bla, 10);
+		pos123++;
+	} while ((ne = strtok(NULL, " \0")) != NULL);
+	NUMboth[pos123+1] = 0;
+
 	int m = 0;
-	long NUMsmallest = 0;
+	////////////
 
-	for (m = 0; NUMinput[m] != 0; m++) {
-		NUM = NUMinput[m];
-		if (m == 0) {NUMsmallest = NUM;}
-		for (int k = 0; k < 7; k++) {
-			int l=0;
-			while (1) {
-				if (pmap[k][l].to == 0 && pmap[k][l].from == 0 &&
-						pmap[k][l].range == 0) { break;}
-				if (pmap[k][l].from <= NUM) {
-					printf("LESS\n");
-					if (pmap[k][l].from + pmap[k][l].range >= NUM) {
-						NUM = NUM - pmap[k][l].from + pmap[k][l].to;
-						break;
-					}
-				} else {printf("greatet...");}
-				l++;
-			}
-		}
-		printf("Location[%d]: %ld\n", m, NUM);
-
-		if (NUMsmallest > NUM) {NUMsmallest = NUM;}
+	unsigned long long NUMsmall = 99999999999999;
+	unsigned long long NUM = 0;;
+	for (m = 0; NUMboth[m] != 0; m++) {
+		//printf("m is %d\n", m);
+		NUM = NUMboth[m];
+		unsigned long long retNUM1 = getLocation(NUM);
+		if (retNUM1 < NUMsmall) {NUMsmall = retNUM1;}
 	}
-	//find smallest
-	
-	printf("Last Location: %ld\n", NUM);
-	printf("Location Smallest: %ld\n", NUMsmallest);
-	fflush(stdin); fflush(stdout);
-	fclose(a);
-	//printf("*** Score:  %d ****\n", score);
-	printf("%d", linenum);
-	fflush(stdout); dup2(fd, 1);
-	printf("**ans: %ld\n", NUMsmallest);
-}
 
+	fflush(stdout); dup2(fd, 1);
+	printf("**ans: %llu\n", NUMsmall);
+}
+unsigned long long getLocation(unsigned long long in) {
+	unsigned long long tmp1 = lookup(in, 0);
+	unsigned long long tmp2 = lookup(tmp1, 1);
+	unsigned long long tmp3 = lookup(tmp2, 2);
+	unsigned long long tmp4 = lookup(tmp3, 3);
+	unsigned long long tmp5 = lookup(tmp4, 4);
+	unsigned long long tmp6 = lookup(tmp5, 5);
+	unsigned long long tmp7 = lookup(tmp6, 6);
+	return tmp7;
+}
+unsigned long long lookup(unsigned long long in, int mppos) {
+	for (int i = 0; i < pmapNum[mppos]; i++) {
+		if (in >= pmap[mppos][i].from  && in <= pmap[mppos][i].from+pmap[mppos][i].range) {
+			return pmap[mppos][i].to + in - pmap[mppos][i].from;
+		} else {
+			continue;
+		}
+	}
+	return in;
+}
