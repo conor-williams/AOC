be53676d4304a93b3e2eb1ebe30ff779db315361
conor-williams <conor.williams@gmail.com>
2025-01-08 18:56:12 +0000

    speedy + no debug

--- ALL.2018/day5.2.c
+++ ALL.2018/day5.2.c
@@ -3,97 +3,74 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <string>
 
+using namespace std;
+
+#include <unistd.h>
+
+#define getchar()
+int ReplaceString(string &subject, const string search, const string replace);
+string rep;
 int main(int argc, char **argv)
 {
-        printf("%d", argc); printf("%s", argv[1]); fflush(stdout);
+	printf("%d", argc); printf("%s", argv[1]); fflush(stdout);
 
-        FILE * a = fopen(argv[1], "r"); printf("2018 Day5.1\n"); fflush(stdout);
+	FILE * a = fopen(argv[1], "r"); printf("2018 Day5.1\n"); fflush(stdout);
 
+	fflush(stdout); int fd = dup(1); close(1);
 	int SIZE = 60000;
-        char line1[SIZE];
+	char line1[SIZE];
 
-int leny = 0;
-int min = SIZE;
-while (1) 
-{
-        fgets(line1, SIZE, a);
-	if (feof(a)) break;
- 	printf("line1 %s\n", line1);
- 	line1[strlen(line1)-1] = '\0';
-	leny++;
-}
-	char newString[SIZE];
-	int newStringPos = 0;
-{
-	int char1 = 'a';
-	int char2 = 'A';
-	int tot = 0;
-	char line1saf[SIZE];
-	strcpy(line1saf, line1);
-	while ((int) char1 <=(int)'z') {
-		tot = 0;
-		newStringPos = 0;
-		strcpy(line1, line1saf);
-		printf("here2"); fflush(stdout);
-		//memset(newString, 0, sizeof(newString));
-		printf("here3"); fflush(stdout);
-		for (int k = 0; k < strlen(line1); k++) {
-			if (line1[k] == char1 || line1[k] == char2) {
-			} else {
-				newString[newStringPos] = line1[k]; newStringPos++;
-				tot++;
-			}
-		}
-		printf("here4"); fflush(stdout);
-		printf("%c%c: %d\n", char1, char2, tot);
-		if (tot < min) {min = tot;}
-		char1++;
-		char2++;
-		
-		newString[newStringPos] = '\0';	
-		printf("newString: %s\n", newString); 
-		
-		for (int i = 0; i < strlen(newString) -1; i++) {
-			if (i < 0) {i = 0;}
-			int g = 0;
-			for (g = i; g >= 0; g--) {
-				if (newString[g] != '0') {break;}
-			}
-			int f = 0;
-			if (newString[g] == '0') {
-				f = 0;
-				for (f = g; f <strlen(newString) -1; f++) {
-					if (newString[f] != '0') {break;}
-				}
-
-			} else {f = g;}
-			int s;
-			for (s = f+1; s < strlen(newString); s++) {
-				if (newString[s] != '0') {break;}
-			}
-
-			if (islower((int)newString[f]) && isupper((int)newString[s])) {
-				if (newString[f] == tolower(newString[s])) {
-					//printf("here1... newString[f] %c V %c newString[s]", newString[f], newString[s]);
-					newString[f] = '0'; newString[s] = '0';
-					//printf("after1 [%s]\n", newString);
-				}
-			} else if (isupper((int)newString[f]) && islower((int)newString[s])) {
-				if (newString[f] == toupper(newString[s])) {
-					//printf("here2... newString[f] %c V %c newString[s]", newString[f], newString[s]);
-					newString[f] = '0'; newString[s] = '0';
-					//printf("after2 [%s]\n", newString);
-				}
-			}
-		}
-		int count = 0;
-		for (int i = 0; i < strlen(newString); i++) {
-			if (newString[i] != '0') {count++;}
-		}
-		if (count < min) {min = count;}
-	//	printf("***polymer len [%d]\n", count);
+	int leny = 0;
+	while (1) 
+	{
+		fgets(line1, SIZE, a);
+		if (feof(a)) break;
+		printf("line1 %s\n", line1);
+		line1[(int)strlen(line1)-1] = '\0';
+		leny++;
 	}
+
+	//rep = line1;
+	int min = 99999999;
+	for (int kk = 0; kk<26; kk++) {
+		rep = line1;
+		string se1; se1.push_back(65+kk);
+		ReplaceString(rep, se1, "");
+		string se2; se2.push_back(97+kk);
+		ReplaceString(rep, se2, "");
+ag:
+		int mark =0;
+		for (int i = 0; i < 26; i++) {
+			char se1[4];
+			sprintf(se1, "%c%c", 65+i, 97+i);	
+			int ch = ReplaceString(rep, se1, "");
+			if (ch == 0) {mark=1;}
+			char se2[4];
+			sprintf(se2, "%c%c", 97+i, 65+i);	
+			printf("se2 %s\n", se2); printf("se1 %s\n", se1);
+			ch = ReplaceString(rep, se2, "");
+			if (ch == 0) {mark=1;}
+		}
+		if (mark == 1) {goto ag;}
+
+		if ((int)rep.length() < min) {min = (int)rep.length();}
+	}
+
+	fflush(stdout); dup2(fd, 1);
+	printf("**ans: %d\n", min);
 }
-	printf("***polymer len MIN [%d]\n", min);
+
+
+int ReplaceString(string &subject, const string search, const string replace) {
+	size_t pos = 0;
+	int none = 1;
+	while ((pos = subject.find(search, pos)) != string::npos) {
+		subject.replace(pos, search.length(), replace);
+		//pos += replace.length();
+		pos--;
+		if (none == 1) {none = 0;}
+	}
+	return none;
 }
--- ALL.2018/day5.c
+++ ALL.2018/day5.c
@@ -3,70 +3,66 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <string>
 
+using namespace std;
+
+#include <unistd.h>
+
+#define getchar()
+int ReplaceString(string &subject, const string search, const string replace);
+string rep;
 int main(int argc, char **argv)
 {
-        printf("%d", argc); printf("%s", argv[1]); fflush(stdout);
+	printf("%d", argc); printf("%s", argv[1]); fflush(stdout);
 
-        FILE * a = fopen(argv[1], "r"); printf("2018 Day5.1\n"); fflush(stdout);
+	FILE * a = fopen(argv[1], "r"); printf("2018 Day5.1\n"); fflush(stdout);
 
+	fflush(stdout); int fd = dup(1); close(1);
 	int SIZE = 60000;
-        char line1[SIZE];
+	char line1[SIZE];
 
-int leny = 0;
-while (1) 
-{
-        fgets(line1, SIZE, a);
-	if (feof(a)) break;
- 	printf("line1 %s\n", line1);
- 	line1[strlen(line1)-1] = '\0';
-	leny++;
-}
-	
-	while(1) {
-		int change = 0;
-		for (int i = 0; i < strlen(line1) -1; i++) {
-				if (i < 0) {i = 0;}
-				int f = 0;
-				for (f = 0; f < strlen(line1) - 1; f++) {
-					if (line1[f] != '0') {break;}
-				}
-				int s;
-				for (s = f+1; s < strlen(line1) - 1; s++) {
-					if (line1[s] != '0') {break;}
-				}
-			       if (islower((int)line1[i]) && isupper((int)line1[i+1])) {
-				if (line1[f] == tolower(line1[s])) {
-//					printf("here1... line1[i] %c V %c line1[i+1]", line1[i], line1[i+1]);
-					line1[f] = '0'; line1[s] = '0';
-					/*int j = 0;
-					for (j = i; j < strlen(line1)-2; j++) {
-						line1[j] = line1[j+2];
-					}
-					line1[j] = '\0';
-					*/
-//			printf("after1 [%s]\n", line1);
-					//i = i - 3;
-					change=1;
-				}
-			} else if (isupper((int)line1[i]) && islower((int)line1[i+1])) {
-				if (line1[i] == toupper(line1[i+1])) {
-//					printf("here2... line1[i] %c V %c line1[i+1]", line1[i], line1[i+1]);
-					line1[f] = '0'; line1[s] = '0';
-					/*int j = 0;
-					for (j = i; j < strlen(line1)-2; j++) {
-						line1[j] = line1[j+2];
-					}
-					line1[j] = '\0';
-					*/
-//			printf("after2 [%s]\n", line1);
-					i = i - 3;
-					change=1;
-				}
-			}
-		}
-		if (change == 0) {break;}
+	int leny = 0;
+	while (1) 
+	{
+		fgets(line1, SIZE, a);
+		if (feof(a)) break;
+		printf("line1 %s\n", line1);
+		line1[(int)strlen(line1)-1] = '\0';
+		leny++;
 	}
-	printf("***polymer len [%d]\n", (int)strlen(line1));
+
+	rep = line1;
+ag:
+	int mark =0;
+	for (int i = 0; i < 26; i++) {
+		char se1[4];
+		sprintf(se1, "%c%c", 65+i, 97+i);	
+		int ch = ReplaceString(rep, se1, "");
+		if (ch == 0) {mark=1;}
+		char se2[4];
+		sprintf(se2, "%c%c", 97+i, 65+i);	
+		printf("se2 %s\n", se2); printf("se1 %s\n", se1);
+		ch = ReplaceString(rep, se2, "");
+		if (ch == 0) {mark=1;}
+	}
+	if (mark == 1) {goto ag;}
+
+	printf("rep: %s\n", rep.c_str());
+
+	fflush(stdout); dup2(fd, 1);
+	printf("**ans: %d\n", (int)rep.length());
 }
 
+
+int ReplaceString(string &subject, const string search, const string replace) {
+	size_t pos = 0;
+	int none = 1;
+	while ((pos = subject.find(search, pos)) != string::npos) {
+		subject.replace(pos, search.length(), replace);
+		//pos += replace.length();
+		pos--;
+		if (none == 1) {none = 0;}
+	}
+	return none;
+}
