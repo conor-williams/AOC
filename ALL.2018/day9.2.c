f0f43840389c0643fc68a1decb3e19b1463f5ef6
conor-williams <conor.williams@gmail.com>
2025-01-08 20:29:52 +0000

    speedy with deque + no debug

--- ALL.2018/day9.2.c
+++ ALL.2018/day9.2.c
@@ -3,19 +3,56 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <deque>
+#include <string>
+#include <iostream>
 
+using namespace std;
+
+#include <unistd.h>
+
+#define getchar()
 #undef _DEBUG_
-#define SIZE 40000
+#define SIZE 200
 char line1[SIZE];
 unsigned long tot = 0;
-#define cSIZE 8000000
-int circle[cSIZE] = {0};
+#define cSIZE 3600000000
+//int circle[cSIZE] = {0};
+deque <int> circle;
 void play(int players, int lastMarble);
+//compile: -Wl,--stack,999777666
+int fd;
+
+void rotateAnti(int d);
+void rotateAnti(int d)
+{
+    for (int i = 0; i < d; i++) {
+        int val = circle.front();
+        circle.pop_front();
+        circle.push_back(val);
+    }
+    // Print the rotated array
+    //for (auto ii: circle) { cout << " " << ii; }
+    //cout << endl;
+}
+void rotateClock(int d);
+void rotateClock(int d)
+{
+    for (int i = 0; i < d; i++) {
+        int val = circle.back();
+        circle.pop_back();
+        circle.push_front(val);
+    }
+    // Print the rotated array
+    //for (auto ii: circle) { cout << " " << ii; }
+    //cout << endl;
+}
 int main(int argc, char **argv)
 {
 	printf("%d", argc); printf("@%s", argv[1]); fflush(stdout);
-	FILE * a = fopen(argv[1], "r"); printf("2018 Day9.1\n"); fflush(stdout);
+	FILE * a = fopen(argv[1], "r"); printf("2018 Day9.2\n"); fflush(stdout);
 
+	fflush(stdout); fd = dup(1); close(1);
 	int leny = 0;
 	while (1)
 	{
@@ -25,74 +62,58 @@ int main(int argc, char **argv)
 		line1[strlen(line1)-1] = '\0';
 		int players, lastMarble;
 		sscanf(line1, "%d players; last marble is worth %d points", &players, &lastMarble);
+		lastMarble *= 100;
 		printf("players: %d lastMarble: %d\n", players, lastMarble);
-		play(players, lastMarble);
-		leny++;
+		getchar();
+		play(players, lastMarble); leny++;
 	}
 	fclose(a);
-	printf("**tot %lu\n", tot);
+
+	//printf("**tot %lu\n", tot);
 }
 
 void play(int players, int lastMarble) {
 	//circle[0] = 0;
-	circle[0] = 2;
-	circle[1] = 1; //len2 //player2
-	circle[2] = 3; //len3 //player2
+	//circle[0] = 2;
+	//circle[1] = 1; //len2 //player2
+	//circle[2] = 3; //len3 //player2
+	circle.clear();
+
+	circle.push_back(2);
+	circle.push_back(1);
+	circle.push_back(3);
+	circle.push_back(0);
+	circle.push_back(4);
 
 	unsigned long long score[450] = {0};
-	for (int z = 0; z < 450; z++) {
-		score[z] = 0;
-	}
-	int curpos = 2;
-	int origI = 3;
-	int tmpcircle[cSIZE] = {0};
-	for (int i = 4; i < cSIZE; i++) {
-		if (origI + 1000 > cSIZE) {printf("ERROR"); exit(0);}
+	int origI = 4;
+	while (1) {
 		origI++;
 		if (origI > lastMarble) {break;}
+
 		int pl = origI % players;
 		if ((origI % 23) != 0) {
-			curpos = (curpos+2) % (i);
-			tmpcircle[curpos] = origI;
-			for (int q = curpos+1; q < i+1; q++) {
-				tmpcircle[q] = circle[q-1];
-			}
-			for (int r = curpos; r < i+1; r++) {
-				circle[r] = tmpcircle[r];
-			}
+			rotateAnti(1);
+			circle.push_back(origI);
 		} else {
-			score[pl] += origI;
-			curpos = ((curpos+i)-7) % (i);
-			score[pl] += circle[curpos];
+			rotateClock(7);
+			rotateClock(1);
+			int val = circle.front();
+			circle.pop_front();
+			rotateAnti(1);
+			score[pl] += (origI) + val;
+		//	printf("origI: %d val: %d\n", origI, val);
 
-			for (int r = curpos; r < i-1; r++) {
-				circle[r] = circle[r+1];
-			}
-			i-=2;
 		}
-#ifdef _DEBUG_
-		printf("[%d]   0  ", pl);
-		for (int j = 0; j < i; j++) {
-			if (j == curpos) {
-				printf("(%d)", circle[j]);
-			} else {
-				printf(" %d ", circle[j]);
-			}
-		}
-		if (twothree == 1) {getchar();}
-		printf("\n");
-#endif
-		if (lastMarble > 7000000 && (origI % (lastMarble/100)) == 0) {
-			unsigned long long max = 0;
-			for (int s = 0; s < players; s++) {
-				if (score[s] > max) {max = score[s];}
-			}
-			printf("INTERIM SCORE (%d): %llu (c.size: %d)\n", (origI/(lastMarble/100)), max, origI);
-		}
+
 	} 
 	unsigned long long max = 0;
 	for (int s = 0; s < players; s++) {
 		if (score[s] > max) {max = score[s];}
 	}
+
 	printf("MAX SCORE %llu\n", max);
+	fflush(stdout); dup2(fd, 1);
+	printf("*ans: %llu\n", max);
+	close(1);
 }
--- ALL.2018/day9.c
+++ ALL.2018/day9.c
@@ -3,116 +3,113 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <deque>
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 #include <unistd.h>
 
 #define getchar()
 #undef _DEBUG_
-#define SIZE 40000
+#define SIZE 200
 char line1[SIZE];
 unsigned long tot = 0;
-#define cSIZE 8000000
-int circle[cSIZE] = {0};
+deque <int> circle;
+
 void play(int players, int lastMarble);
-//compile: -Wl,--stack,999777666
 int fd;
+
+void rotateAnti(int d);
+void rotateAnti(int d)
+{
+    for (int i = 0; i < d; i++) {
+        int val = circle.front();
+        circle.pop_front();
+        circle.push_back(val);
+    }
+    // Print the rotated array
+    //for (auto ii: circle) { cout << " " << ii; }
+    //cout << endl;
+}
+void rotateClock(int d);
+void rotateClock(int d)
+{
+    for (int i = 0; i < d; i++) {
+        int val = circle.back();
+        circle.pop_back();
+        circle.push_front(val);
+    }
+    // Print the rotated array
+    //for (auto ii: circle) { cout << " " << ii; }
+    //cout << endl;
+}
 int main(int argc, char **argv)
 {
 	printf("%d", argc); printf("@%s", argv[1]); fflush(stdout);
 	FILE * a = fopen(argv[1], "r"); printf("2018 Day9.1\n"); fflush(stdout);
 
-	fflush(stdout); int fd = dup(1); close(1);
+	fflush(stdout); fd = dup(1); close(1);
 	int leny = 0;
 	while (1)
 	{
 		fgets(line1, SIZE-1, a);
 		if (feof(a)) break;
-		//printf("line1 %s\n", line1);
 		line1[strlen(line1)-1] = '\0';
 		int players, lastMarble;
 		sscanf(line1, "%d players; last marble is worth %d points", &players, &lastMarble);
 		printf("players: %d lastMarble: %d\n", players, lastMarble);
-		play(players, lastMarble);
-		leny++;
+		getchar();
+		play(players, lastMarble); leny++;
 	}
 	fclose(a);
 
-	fflush(stdout); dup2(fd, 1);
-	printf("**tot %lu\n", tot);
+	//printf("**tot %lu\n", tot);
 }
 
 void play(int players, int lastMarble) {
 	//circle[0] = 0;
-	circle[0] = 2;
-	circle[1] = 1; //len2 //player2
-	circle[2] = 3; //len3 //player2
+	//circle[0] = 2;
+	//circle[1] = 1; //len2 //player2
+	//circle[2] = 3; //len3 //player2
+	circle.clear();
+
+	circle.push_back(2);
+	circle.push_back(1);
+	circle.push_back(3);
+	circle.push_back(0);
+	circle.push_back(4);
 
 	unsigned long long score[450] = {0};
-	int curpos = 2;
-	int origI = 3;
-	for (int i = 4; i < cSIZE; i++) {
-		if (origI + 1000 > cSIZE) {printf("ERROR"); exit(0);}
+	int origI = 4;
+	while (1) {
 		origI++;
 		if (origI > lastMarble) {break;}
+
 		int pl = origI % players;
 		if ((origI % 23) != 0) {
-			curpos = (curpos+2) % (i);
-			//printf("curpos %d\n", curpos);
-			int tmpcircle[cSIZE];
-			for (int z = 0; z < curpos; z++) {
-				tmpcircle[z] = circle[z];
-			}
-			tmpcircle[curpos] = origI;
-			for (int q = curpos+1; q < i+1; q++) {
-				tmpcircle[q] = circle[q-1];
-			}
-			for (int r = 0; r < i; r++) {
-				circle[r] = tmpcircle[r];
-			}
+			rotateAnti(1);
+			circle.push_back(origI);
 		} else {
-			score[pl] += origI;
-			curpos = ((curpos+i)-7) % (i);
-			score[pl] += circle[curpos];
-			//printf("player %d points %d aiming for %d\n", pl, score[pl], points); break;
+			rotateClock(7);
+			rotateClock(1);
+			int val = circle.front();
+			circle.pop_front();
+			rotateAnti(1);
+			score[pl] += (origI) + val;
+		//	printf("origI: %d val: %d\n", origI, val);
 
-			int tmpcircle[cSIZE];
-			for (int z = 0; z < curpos-1; z++) {
-				tmpcircle[z] = circle[z];
-			}
-			for (int q = curpos; q < i+1; q++) {
-				tmpcircle[q] = circle[q+1];
-			}
-			for (int r = 0; r < i-1; r++) {
-				circle[r] = tmpcircle[r];
-			}
-			i-=2;
 		}
-#ifdef _DEBUG_
-		printf("[%d]   0  ", pl);
-		for (int j = 0; j < i; j++) {
-			if (j == curpos) {
-				printf("(%d)", circle[j]);
-			} else {
-				printf(" %d ", circle[j]);
-			}
-		}
-		if (twothree == 1) {getchar();}
-		printf("\n");
-#endif
-		if (origI % 10000 == 0) {
-			unsigned long long max = 0;
-			for (int s = 0; s < players; s++) {
-				if (score[s] > max) {max = score[s];}
-			}
-			printf("INTERIM SCORE %llu\n", max);
-		}
+
 	} 
 	unsigned long long max = 0;
 	for (int s = 0; s < players; s++) {
 		if (score[s] > max) {max = score[s];}
 	}
 
-	fflush(stdout); dup2(fd, 1);
 	printf("MAX SCORE %llu\n", max);
-	exit(0);
+	fflush(stdout); dup2(fd, 1);
+	printf("*ans: %llu\n", max);
+	close(1);
 }
