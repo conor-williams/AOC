ff8cc9d3ea69c1eb419b70ecee6bc70f4b4eba9d
conor-williams <conor.williams@gmail.com>
2025-01-10 20:34:45 +0000

    working well - a tad slow - no debug

--- ALL.2023/day17.c
+++ ALL.2023/day17.c
@@ -3,174 +3,170 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <assert.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
 
-int lenx, leny;
-#define DAY "2023 day17 part1\n"
-#define _DEBUG_
-int tot;
-struct posit {
-	int x;	
-	int y;
-	int dir;
-	long tot;
+#include <unistd.h>
+
+#define getchar()
+/*
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+
+
+struct hash_function
+{
+size_t operator()(const tuple<int , int , int, int>&x) const
+{
+unsigned int hash = get<0>(x); hash *= 37; hash += get<1>(x); hash *= 27; hash += get<2>(x); hash *= 27; hash += get<3>(x); return hash;
+}
 };
-struct posit savearray[100000];
-int savearrayLen;
 
-char puzzle[200][200];
-char puzzlecopy[200][200];
-void printpuzzle();
-void printpuzzlecopy();
-void everyPath(int sX, int sY, int sD, long tot);
-void saveit(int x1, int y1, int dir1, long tot);
-int existsP(int x1, int y1, int dir1, long tot);
+*/
 
+
+void next (int x, int y, int ex, int ey, int path, int dir, int timesInDir);
+
+FILE *a;
+#define LINE 1000
+#define getchar()
+void sigfunc(int a) { printf("[[ %s ]]\n", "signal hand..\n"); }
+int lenx;
+int leny;
+
+char grid[200][200];
+struct al_s {
+	int path[4][5];
+	//int timesInDir[5];
+};
+
+struct al_s already[200][200];
+
+int minPath = 0;
 int main(int argc, char **argv)
 {
-	savearrayLen = 0;
-	tot = 0;lenx = 0; leny = 0;
-        printf("%d", argc); printf("%s", argv[1]); fflush(stdout);
-        FILE * a = fopen(argv[1], "r"); 
-	printf(DAY); fflush(stdin); fflush(stdout);
-       
-        char line1[200];
-while(1) {
-        fgets(line1, 150, a);
-        if (feof(a)) break;
-	line1[strlen(line1) -1]='\0';
-	lenx = strlen(line1);
-#ifdef _DEBUG_
-	printf("LINE: %s\n", line1);
-#endif
-	strcpy(puzzle[leny], line1);
-        leny++;
-}
-fclose(a);
-printf("lenx: %d leny: %d\n", lenx, leny);
+	printf("SLOW: approx < 1min\n");
+	clock_t start, end; double cpu_time_used; 
+	start = clock();
+	//DO
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("        time: %f seconds\n", cpu_time_used);
 
+	//signal(SIGTSTP, &sigfunc);
+	signal(SIGQUIT, &sigfunc);
+	printf("%d", argc); printf("%s\n", argv[1]); fflush(stdout);
 
-everyPath(0, 0, 0, 0);
-printpuzzle();
-for (int i = 0; i < savearrayLen;i++) {
-	//printf("%d, %d, tot: %d\n", savearray[i].x, savearray[i].y, savearray[i].tot);	
-	if ((savearray[i].x) == lenx -1 && (savearray[i].y == leny - 1)) {
-		printf("****tot path %ld\n", savearray[i].tot);
+	a = fopen(argv[1], "r"); if (a == NULL) {printf("BAD file %s\n", argv[1]); exit(0);}
+	printf("2024 Day 17 Part 1\n"); fflush(stdout);
+
+	fflush(stdout); int fd = dup(1); close(1);
+	char line1[LINE];
+
+	leny = 0;
+	while (1) {
+		fgets(line1, LINE-1, a);
+		if (feof(a)) break;
+		line1[strlen(line1)-1] = '\0';
+		printf("LINE: %s\n", line1);
+		strcpy(grid[leny], line1);
+		leny++;
 	}
-}
-}
-void saveit(int x1, int y1, int dir1, long tot)
-{
-	int found = 0;
-if (dir1 == 2 || dir1 == 3) {
-	dir1 -= 2;
-}
-	for (int i = 0; i < savearrayLen; i++) {
-		if (savearray[i].x == x1 && savearray[i].y == y1 && savearray[i].dir == dir1) {
-			found = 1;
-			if (tot < savearray[i].tot) { 
-				savearray[i].tot = tot;
-			}
-			break;
-		}
+	fclose(a);
+	lenx = (int)strlen(grid[0]);
+	int sx = 0; int sy = 0;
+	int ex = lenx-1; int ey = leny-1;
+
+	memset(already, 0, sizeof(already));
+	printf("end: %c\n", grid[ey][ex]); getchar();
+	printf("start: %c\n", grid[sy][sx]); getchar();
+
+	int minmin = 999999999;
+	{
+		minPath = leny*lenx*9;
+		memset(already, 0, sizeof(already));
+		next(sx, sy, ex, ey, - (grid[sy][sx] -48) , 1, 1);
+		printf("gone east minPath %d\n", minPath);
+		if (minPath < minmin) {minmin = minPath;}
+
+		//minPath = lenx*leny*9; 
+		memset(already, 0, sizeof(already));
+		next(sx, sy, ex, ey, - (grid[sy][sx] -48) , 2, 1);
+		printf("**gone south minPath: %d\n", minPath);
+		if (minPath < minmin) {minmin = minPath;}
 	}
-	if (!found) {
-		//printf("adding.to save array..\n");
-		savearray[savearrayLen].x = x1;
-		savearray[savearrayLen].tot = tot;
-		savearray[savearrayLen].y = y1;
-		savearray[savearrayLen].dir = dir1;
-		savearrayLen++;
+
+	fflush(stdout); dup2(fd, 1);
+	printf("**ans: %d\n", minmin);
+}
+
+void next (int x, int y, int ex, int ey, int path, int dir, int timesInDir) {
+	if (x > lenx -1 || y > leny -1 || x < 0 || y < 0) {
+		return;
 	}
-	//printf("savearrayLen now  %d\n", savearrayLen); fflush(stdout);
-	
-}
-int existsP(int x1, int y1, int dir1, long tot)
-{
-	int found = 0;
-if (dir1 == 2 || dir1 == 3) {
-	dir1 -= 2;
-}
-	for (int i = 0; i < savearrayLen; i++) {
-		if (savearray[i].x == x1 && savearray[i].y == y1 && savearray[i].dir == dir1) {
-			if (savearray[i].tot <= tot) {
-				//printf("already there.existsP..\n");
-				found = 1;
-			} else if (savearray[i].tot > tot) {
-			}
-			break;
-		}
-	}
-	return found;
-}
-void everyPath(int sX, int sY, int sD, long tot)
-{
-	//printf("in everyPath %d %d %d %ld\n", sX, sY, sD, tot);
-	if (sX == lenx-1 && sY == leny -1) {
-		//printf("We reached end point - getchar tot %ld\n", tot); fflush(stdout); //getchar();
-		//printf("****tot %d\n", tot);
+	if (timesInDir > 3) {return;}
+
+	path += grid[y][x] - 48;
+	//printf("path is %d\n", path);
+	if (path >= minPath) {return;}
+
+	if (x == ex && y == ey) {
+		///printf("got end path %d\n", path);
+		if (path < minPath) {minPath = path;}
 		return;
 	}
 
-	for (int i = 1; i <= 3; i++) {
-		for (int z = -1; z <= 1; z++) {
-		    if (z != 0) {
-			int startx = sX; int starty = sY; long starttot = tot; int stop = 0; int dir = ((sD + z) + 4) % 4;
-			switch (dir) {
-				case 0:
-					if (starty - i <= -1) {stop = 1;} else {starty -= i;
-						for (int j = 0; j < i; j++) {
-							starttot += (int)(puzzle[starty+j][startx] - '0');
-						}
-					}
-					break;
-				case 1:
-					if (startx + i >= lenx) {stop = 1;} else {startx += i;
-						for (int j = 0; j < i; j++) {
-							starttot += puzzle[starty][startx - j] - '0';
-						}
+	if (already[y][x].path[dir][timesInDir] == 0 || path < already[y][x].path[dir][timesInDir])
+	{
+		already[y][x].path[dir][timesInDir] = path;
 
-					}
-					break;
-				case 2:
-					if (starty + i >= leny) {stop = 1;} else {starty += i;
-
-						for (int j = 0; j < i; j++) {
-							starttot += puzzle[starty -j][startx] - '0';
-						}
-
-					}
-					break;
-				case 3:
-					if (startx - i <= -1) {stop = 1;} else {startx -= i;
-						for (int j = 0; j < i; j++) {
-							starttot += puzzle[starty][startx + j] - '0';
-						}
-
-					}
-					break;
-				default:
-					printf("ERROR unknown dir getchar()\n"); getchar();
-					break;
-			}
-
-			if (stop == 1) {
-
-			} else {
-			 	if (!existsP(startx, starty, dir, starttot)) {saveit(startx, starty, dir, starttot); everyPath(startx, starty, dir, starttot);}
-#ifdef _DEBUG_
-				//printf("calling %d,%d Plus %d\n", startx, starty, (int)(puzzle[starty][startx] - '0')); getchar();
-				//char savedchar = puzzle[starty][startx]; puzzle[starty][startx] = 'Q'; printpuzzle(); getchar(); puzzle[starty][startx] = savedchar;
-#endif
-				
-			}
+		if (dir == 0) {
+			next(x, y-1, ex, ey, path, 0, timesInDir+1);
+			next(x-1, y, ex, ey, path, 3, 1);
+			next(x+1, y, ex, ey, path, 1, 1);
+		} else if (dir == 1) {
+			next(x+1, y, ex, ey, path, 1, timesInDir+1);
+			next(x, y-1, ex, ey, path, 0, 1);
+			next(x, y+1, ex, ey, path, 2, 1);
+		} else if (dir == 2) {
+			next(x, y+1, ex, ey, path, 2, timesInDir+1);
+			next(x+1, y, ex, ey, path, 1, 1);
+			next(x-1, y, ex, ey, path, 3, 1);
+		} else if (dir == 3) {
+			next(x-1, y, ex, ey, path, 3, timesInDir+1);
+			next(x, y+1, ex, ey, path, 2, 1);
+			next(x, y-1, ex, ey, path, 0, 1);
 		}
-	    }
 	}
+}
 
-}
-void printpuzzle()
-{
-    for (int y = 0; y < leny; y++) {
-	printf("%s\n", puzzle[y]);
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+		/*
+		if (already[y][x].path == 0) {already[y][x].path = path; already[y][x].timesInDir[dir] = timesInDir; }
+		if (path < already[y][x].path) {already[y][x].path = path;}
+		if (path > already[y][x].path) {
+			if (timesInDir < already[y][x].timesInDir[dir]) {already[y][x].timesInDir[dir] = timesInDir;}
+		}
+		if (already[y][x].timesInDir[dir] == 0) {already[y][x].timesInDir[dir] = timesInDir;}
+		*/
