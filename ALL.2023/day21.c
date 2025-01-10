fb0cbb73f3643b99df2cf96bfd5617b0b78ecbd8
conor-williams <conor.williams@gmail.com>
2025-01-10 21:23:46 +0000

    working well + no debug + clear answer

--- ALL.2023/day21.c
+++ ALL.2023/day21.c
@@ -3,121 +3,122 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <sys/time.h>
-#include <signal.h>
+#include <assert.h>
 #include <unistd.h>
-int leny;
-int lenx;
-#define DAY "2023 Day21 Part1"
+#include <signal.h>
+#include <time.h>
 
-char puzzle[132][132];
-int puzzlealready[132][132];
-#undef _DEBUG_ 
-int gcountO;
+
+#include <unistd.h>
+#define assert(asf)
+#define getchar()
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+
+
+struct hash_function
+{
+size_t operator()(const tuple<int , int , int, int>&x) const
+{
+unsigned int hash = get<0>(x); hash *= 37; hash += get<1>(x); hash *= 27; hash += get<2>(x); hash *= 27; hash += get<3>(x); return hash;
+}
+};
+
+map <pair <int, int>, int> mp;	
+map <pair <int, int>, int> mpTmp;	
+void move(int x, int y);
+
 FILE *a;
-#define MAX_STEPS 65
-void findstart(int *startx, int *starty);
-void printpuzzle();
-int countOinpuzzle();
-void nextandcheck(int startx, int starty, int steps);
-int numspawn;
+#define LINE 1000
+//#define getchar()
+void sigfunc(int a) { printf("[[ %s ]]\n", "signal hand..\n"); }
+int lenx;
+int leny;
 
+char grid[140][140];
 int main(int argc, char **argv)
 {
+	clock_t start, end; double cpu_time_used; 
+	start = clock();
+	//DO
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("        time: %f seconds\n", cpu_time_used);
 
-        printf("%d\n", argc); printf("%s\n", argv[1]); fflush(stdin); fflush(stdout);
-        a = fopen(argv[1], "r"); 
-	printf(DAY); printf("\n"); fflush(stdin); fflush(stdout);
-        char line1[1000];
+	//signal(SIGTSTP, &sigfunc);
+	signal(SIGQUIT, &sigfunc);
+	printf("%d", argc); printf("%s\n", argv[1]); fflush(stdout);
 
-leny = 0; lenx = 0;  numspawn = 0;
-{
-	for (int x = 0; x < 132; x++) {
-		for (int y = 0; y < 132; y++) {
-			puzzlealready[y][x] = 0;
-		}
+	a = fopen(argv[1], "r"); if (a == NULL) {printf("BAD file %s\n", argv[1]); exit(0);}
+	printf("2023 Day 21 Part1\n"); fflush(stdout);
+
+	fflush(stdout); int fd = dup(1); close(1);
+	char line1[LINE];
+
+	leny = 0;
+	while (1) {
+		fgets(line1, LINE-1, a);
+		if (feof(a)) break;
+		line1[strlen(line1)-1] = '\0';
+		printf("LINE: %s\n", line1);
+		strcpy(grid[leny], line1);
+		leny++;
 	}
-}
-
-while(1) {
-        fgets(line1, 999, a);
-        if (feof(a)) break;
-#ifdef _DEBUG_
-	printf("LINE: %s", line1); fflush(stdout);
-#endif
-fflush(stdout);
-	line1[strlen(line1) -1] = '\0';
-	strcpy(puzzle[leny], line1);
-#ifdef _DEBUG_ 
-	printf("----------------\n");
-#endif
-	leny++;       
-	
-} lenx = strlen(line1);
-int startx = 0; int starty = 0; gcountO = 0;
-{
-	findstart(&startx, &starty);
-	printpuzzle();
-	puzzle[starty][startx] = '.';
-	nextandcheck(startx, starty, 0);
-	printf("*****gcountO %d\n", gcountO);
-
-}
-printpuzzle(puzzle);
-fclose(a);
-}
-void nextandcheck(int startx, int starty, int steps)
-{
-	numspawn++; //printf("numspawn %d\n", numspawn);
-	steps++;
-	if (steps == MAX_STEPS) {	
-		if (puzzle[starty][startx] != '#') {
-			if (puzzlealready[starty][startx] != 1) {
-				gcountO++; puzzlealready[starty][startx] = 1;
-				return;
-			}
-		}
-		return;
-	}
-	if (puzzle[starty][startx] == '#') {return;}
-
-	//puz[starty][startx] = '.';
-
-	if (startx -1 > 0   ) {nextandcheck(startx -1, starty, steps); } else { gcountO++; puzzlealready[starty][startx] = 1;}
-	if (startx +1 < lenx) {nextandcheck(startx +1, starty, steps); } else { gcountO++; puzzlealready[starty][startx] = 1;}
-	if (starty -1 > -1  ) {nextandcheck(startx, starty -1, steps); } else { gcountO++; puzzlealready[starty][startx] = 1;}
-	if (starty +1 < leny) {nextandcheck(startx, starty +1, steps); } else { gcountO++; puzzlealready[starty][startx] = 1;}
-}
-
-
-void findstart(int *startx, int *starty)
-{
-	for (int y = 0; y < leny; y++) {
-		for (int x = 0; x < lenx; x++) {
-			if (puzzle[y][x] == 'S') {
-				*startx = x;
-				*starty = y;
-				return;
+	fclose(a);
+	lenx = (int)strlen(grid[0]);
+	int sx, sy;
+	for (int yy = 0; yy < leny; yy++) {
+		for (int xx = 0; xx < lenx; xx++) {
+			if (grid[yy][xx] == 'S') {
+				sx = xx; sy=yy; grid[yy][xx] = '.'; break;
 			}
 		}
 	}
-}
 
-void printpuzzle()
-{
-	for (int y = 0; y < leny; y++) {
-		for (int x = 0; x < lenx; x++) {
-			if (puzzlealready[y][x] == 1) {
-				printf("%c", 'x');
-			} else {
-				printf("%c", puzzle[y][x]);
-			}
+	int ENDSTEPS = 64;
+	if (argc == 3) {ENDSTEPS = atoi(argv[2]);}
+	mp[{sx, sy}] = 1;
+	for (int ste = 0; ste < ENDSTEPS; ste++) {
+		mpTmp.clear();
+		for (auto it = mp.begin(); it != mp.end(); it++) {
+			int x1 = (*it).first.first;
+			int y1 = (*it).first.second;
+			move(x1, y1);
 		}
-		printf("\n");
+		mp = mpTmp;
+		printf("steps: %d -- plots: %d\n", ste, (int)mp.size());
 	}
-	printf("\n");
+	fflush(stdout); dup2(fd, 1);
+	printf("**ans: %d\n", (int)mp.size());
+}
+void move(int x, int y) 
+{
+	if (y-1>= 0 && grid[y-1][x] != '#') {
+		if (mpTmp.find({x, y-1}) == mpTmp.end()) {
+			mpTmp[{x, y-1}] = 1;
+		}
+	}
+	if (x+1 < lenx && grid[y][x+1] != '#') {
+		if (mpTmp.find({x+1, y}) == mpTmp.end()) {
+			mpTmp[{x+1, y}] = 1;
+		}
+	}
+	if (y+1 < leny && grid[y+1][x] != '#') {
+		if (mpTmp.find({x, y+1}) == mpTmp.end()) {
+			mpTmp[{x, y+1}] = 1;
+		}
+	}
+	if (x-1 >= 0 && grid[y][x-1] != '#') {
+		if (mpTmp.find({x-1, y}) == mpTmp.end()) {
+			mpTmp[{x-1, y}] = 1;
+		}
+	}
 }
