555f5b6ab75c65eb5701fa64f1e7c5dc109748c2
conor-williams <conor.williams@gmail.com>
2025-01-13 16:25:25 +0000

    alternative one faster + no debug + clear answers

---
+++ ALL.2023/day17.2.alternative.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <assert.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <vector>
+#include <algorithm>
+#include <deque>
+#include <unordered_set>
+
+using namespace std;
+#include <unistd.h>
+
+struct hash_function_dummy
+{
+	size_t operator()(const pair<int , int>&x) const
+	{
+		unsigned int hash = x.first; hash *= 37; hash += x.second; hash*= 37; return hash;
+	}
+};
+//windows thinks this files exe is a virus
+unordered_set <pair <int, int>, hash_function_dummy> sese_dummy;
+
+#define getchar()
+int NUMV = 0;
+FILE *a;
+#define LINE 1000
+#define getchar()
+void sigfunc(int a) { printf("[[ %s ]]\n", "signal hand..\n"); }
+void next (int x, int y, int ex, int ey, int path, int dir, int timesInDir, int first);
+int lenx;
+int leny;
+
+char grid[200][200];
+struct al_s {
+	//int path;
+	int path[5][12];
+};
+
+struct al_s already[200][200];
+
+int minPath = 0;
+int main(int argc, char **argv)
+{
+	printf("widen stack\n");
+	printf("SLOW ~20mins\n");
+	clock_t start, end; double cpu_time_used; 
+	start = clock();
+	//DO
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("        time: %f seconds\n", cpu_time_used);
+
+	//signal(SIGTSTP, &sigfunc);
+	signal(SIGQUIT, &sigfunc);
+	printf("%d", argc); printf("%s\n", argv[1]); fflush(stdout);
+	a = fopen(argv[1], "r"); if (a == NULL) {printf("BAD file %s\n", argv[1]); exit(0);}
+	printf("2024 Day 17 Part 2\n"); fflush(stdout);
+
+	fflush(stdout); int fd = dup(1); close(1);
+
+	char line1[LINE];
+
+	leny = 0;
+	while (1) {
+		fgets(line1, LINE-1, a);
+		if (feof(a)) break;
+		line1[strlen(line1)-1] = '\0';
+		printf("LINE: %s\n", line1);
+		strcpy(grid[leny], line1);
+		leny++;
+	}
+	fclose(a);
+	lenx = (int)strlen(grid[0]);
+	int sx = 0; int sy = 0;
+	int ex = lenx-1; int ey = leny-1;
+
+	//memset(already, 0, sizeof(already));
+	printf("end: %c\n", grid[ey][ex]); getchar();
+	printf("start: %c\n", grid[sy][sx]); getchar();
+
+	int minmin = lenx*leny*9;
+	{
+		minPath = lenx*leny*9; 
+		printf("going south...\n");
+		memset(already, 0, sizeof(already));
+		next(sx, sy, ex, ey, -(grid[0][0]-48), 2, 0, 0);
+		printf("**gone south minPath: %d\n", minPath);
+		if (minPath < minmin) {minmin = minPath;}
+
+		//minPath = leny*lenx*9;
+		printf("going east...\n");
+		memset(already, 0, sizeof(already));
+		next(sx, sy, ex, ey, -(grid[0][0] -48), 1, 0, 0);
+		printf("gone east minPath %d\n", minPath);
+		if (minPath < minmin) {minmin = minPath;}
+
+
+	}
+
+	fflush(stdout); dup2(fd, 1);
+	printf("**ans: %d\n", minmin);
+}
+
+void next (int x, int y, int ex, int ey, int path, int dir, int timesInDir, int first) {
+	if (x > lenx -1 || y > leny -1 || x < 0 || y < 0) {
+		return;
+	}
+	if (timesInDir > 10) {return;}
+
+	path += grid[y][x] - 48;
+	//printf("path is %d\n", path);
+	if (path >= minPath) {return;}
+
+	if (x == ex && y == ey) {
+		printf("got an end path %d -- checking\n", path); fflush(stdout);
+		if (timesInDir >= 4)  {
+			printf("timesInDir good %d %d\n", path, timesInDir);
+					
+			if (path < minPath) {
+				minPath = path;
+			}
+		} else {
+			printf("times-in-dir bad %d %d\n", timesInDir, path);
+		}
+		return;
+	}
+
+	if (already[y][x].path[dir][timesInDir] == 0 || path < already[y][x].path[dir][timesInDir]) {
+		already[y][x].path[dir][timesInDir] = path;
+
+		if (dir == 0) {
+			if (timesInDir >= 4 && timesInDir <= 10) {
+				next(x-1, y, ex, ey, path, 3, 1, 1);
+				next(x+1, y, ex, ey, path, 1, 1, 1);
+			}
+			next(x, y-1, ex, ey, path, 0, timesInDir+1, 1);
+		} else if (dir == 1) {
+			if (timesInDir >= 4 && timesInDir <= 10) {
+				next(x, y-1, ex, ey, path, 0, 1, 1);
+				next(x, y+1, ex, ey, path, 2, 1, 1);
+			}
+			next(x+1, y, ex, ey, path, 1, timesInDir+1, 1);
+		} else if (dir == 2) {
+			if (timesInDir >= 4 && timesInDir <= 10) {
+				next(x+1, y, ex, ey, path, 1, 1, 1);
+				next(x-1, y, ex, ey, path, 3, 1, 1);
+			}
+			next(x, y+1, ex, ey, path, 2, timesInDir+1, 1);
+		} else if (dir == 3) {
+			if (timesInDir >= 4 && timesInDir <= 10) {
+				next(x, y+1, ex, ey, path, 2, 1, 1);
+				next(x, y-1, ex, ey, path, 0, 1, 1);
+			}
+			next(x-1, y, ex, ey, path, 3, timesInDir+1, 1);
+		}
+	}
+}
---
+++ ALL.2023/day17.alternative.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <assert.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <string_view>
+#include <vector>
+#include <unistd.h>
+///#define assert(asf)
+//#define getchar()
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+
+
+struct hash_function
+{
+	size_t operator()(const tuple<int , int , int, int, int>&x) const
+	{
+		unsigned int hash = get<0>(x); 
+		hash *= 37;
+		hash += get<1>(x);
+		hash *= 37;
+		hash += get<2>(x);
+		hash *= 37;
+		hash += get<3>(x);
+		hash*= 37;
+		hash += get<4>(x);
+		hash*= 37;
+		return hash;
+	}
+};
+
+
+
+
+FILE *a;
+#define LINE 1000
+//#define getchar()
+void sigfunc(int a) { printf("[[ %s ]]\n", "signal hand..\n"); }
+int lenx;
+int leny;
+priority_queue <tuple<int, int, int, int, int, int>, vector<tuple<int, int, int, int, int, int>>, greater<tuple<int, int, int, int, int, int>>> pr;
+char grid[200][200];
+void runit(int, int);
+int minPath;
+int main(int argc, char **argv)
+{
+	clock_t start, end; double cpu_time_used; 
+	start = clock();
+	//DO
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("        time: %f seconds\n", cpu_time_used);
+
+	//signal(SIGTSTP, &sigfunc);
+	signal(SIGQUIT, &sigfunc);
+	printf("%d", argc); printf("%s\n", argv[1]); fflush(stdout);
+
+	a = fopen(argv[1], "r"); if (a == NULL) {printf("BAD file %s\n", argv[1]); exit(0);}
+	printf("20YY Day 17 Part 1\n"); fflush(stdout);
+
+	fflush(stdout); int fd = dup(1); close(1);
+	char line1[LINE];
+
+	leny = 0;
+	while (1) {
+		fgets(line1, LINE-1, a);
+		if (feof(a)) break;
+		line1[strlen(line1)-1] = '\0';
+		printf("LINE: %s\n", line1);
+		strcpy(grid[leny], line1);
+		leny++;
+	}
+	fclose(a);
+	lenx = (int)strlen(grid[0]);
+
+	//     pa, x, y, dirX, dirY, stepsindir, 
+	int ex = lenx-1; int ey = leny-1;
+
+	minPath = lenx*leny*9;
+	pr.push({0, 0, 0, 0, 1, 0});
+	pr.push({0, 0, 0, 1, 0, 0});
+	runit(ex, ey);
+
+	printf("ex: %d ey : %d\n", ex, ey);
+	fflush(stdout); dup2(fd, 1);
+	printf("**ans: %d\n", minPath);
+}
+void runit(int ex, int ey) {
+	unordered_set <tuple<int, int, int, int, int>, hash_function> se;
+	while (!pr.empty()) {
+		tuple <int, int, int, int, int, int> data = pr.top();
+		pr.pop();
+
+		int path = get<0>(data);
+		if (path > minPath) {continue;}
+		int x = get<1>(data);
+		int y = get<2>(data);
+		int dirX = get<3>(data);
+		int dirY = get<4>(data);
+		int ste = get<5>(data);
+
+		if (se.find({x, y, dirX, dirY, ste}) != se.end()) {continue;}
+		se.insert({x, y, dirX, dirY, ste});
+		if (ste > 3) {continue;}
+		if (x == ex && y == ey) {
+			printf("poss min: %d\n", path); fflush(stdout);
+			if (path < minPath && ste <= 3) {
+				minPath = path;
+				printf("	settingminPath %d ste %d\n", minPath, ste);
+				return;
+			} else {printf("end reached path(%d)V(%d) %d no good\n", path, minPath, ste);}
+			continue;
+		}
+		{//straight or turn
+			pair <int, int> pa[4];
+			pa[0].first = 0;  pa[0].second = -1;
+			pa[1].first = 1;  pa[1].second = 0;
+			pa[2].first = 0;  pa[2].second = 1;
+			pa[3].first = -1; pa[3].second = 0;
+			//
+			//
+			//for dx,dy in (0,1) (1,0) (0,-1) (-1,0)
+			for (int ii = 0; ii < 4; ii++) {
+				if (pa[ii].first - dirX == 0 && pa[ii].second - dirY == 0) {
+					if (ste <= 3) {
+						//same dir
+						int nx = x + dirX;
+						int ny = y + dirY;
+						if (nx < 0 || nx > leny-1 || ny < 0 || ny > leny-1) {
+						} else {
+							pr.push({path+((int)grid[ny][nx]-48), nx, ny, dirX, dirY, ste+1});
+						}
+					}
+				} else if (pa[ii].first + dirX == 0 && pa[ii].second + dirY == 0) {
+					//opposite dir
+					continue;
+				} else {
+					//turn if he has gone at least 4 steps not more than 10
+						int nx = x+ pa[ii].first;	
+						int ny = y+ pa[ii].second;	
+						if (nx < 0 || nx > leny-1 || ny < 0 || ny > leny-1) {
+							continue;
+						} else {
+							pr.push({path+((int)grid[ny][nx]-48), nx, ny, pa[ii].first, pa[ii].second, 1});
+							if (abs(abs(dirX) - abs(pa[ii].first)) != 1) {printf("ERR2"); exit(0);}
+							if (abs(abs(dirY) - abs(pa[ii].second)) != 1) {printf("ERR3"); exit(0);}
+
+						}
+				}
+			}
+		}
+
+	}
+}
