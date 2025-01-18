73b8de73f0fc8022bbbeb0c6b206d4ff179a2799
conor-williams <conor.williams@gmail.com>
2025-01-18 03:23:41 +0000

    missing a $

--- onelinercheckYEAR.sh
+++ onelinercheckYEAR.sh
@@ -2,7 +2,7 @@ year=$1
 day=1;
 while [ $day != 26 ]; do
 	if [ -f $year/day$day/day$day.exe ] ; then
-		year/day$day/day$day.exe $year/day$day/i1.txt
+		$year/day$day/day$day.exe $year/day$day/i1.txt
 	fi 
 	if [ -f $year/day$day/day$day.2.exe ] ; then
 		$year/day$day/day$day.2.exe $year/day$day/i1.txt
