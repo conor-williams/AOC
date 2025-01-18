66d50b1723571ac553bf7482241a773dae1f266b
conor-williams <conor.williams@gmail.com>
2025-01-18 05:15:29 +0000

    a quick mv of day25 2019

--- FIRST.sh
+++ FIRST.sh
@@ -3,6 +3,7 @@
 tar -xvzf input-2015-2014.tar.gz
 sh specialCopy.sh
 sh specialCompile.sh
+mv 2019/day25/*.exe 2019/day25/day25.INTERACTIVE.exe
 sh runALL.sh
 
 ##creates output.2015, output.2016 etc...
