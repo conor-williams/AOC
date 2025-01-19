92cabca4f0c083e16d859beb283a91e426ece9dd
conor-williams <conor.williams@gmail.com>
2025-01-19 06:03:25 +0000

    broken

--- ALL.2022/day11.2.c
+++ ALL.2022/day11.2.c
@@ -73,6 +73,7 @@ int main(int argc, char **argv)
 	printf("%d", argc); printf("%s", argv[1]); fflush(stdout);
 	a = fopen(argv[1], "r"); printf("2022 Day11 part2\n"); fflush(stdout);
 
+	printf("broken\n"); exit(0);
 	fflush(stdout); fd = dup(1); close(1);
 	char line1[1000];
 
