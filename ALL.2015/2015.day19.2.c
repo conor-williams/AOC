f4f90bd39ea58b2e996db714637ba43466eb2492
conor-williams <conor.williams@gmail.com>
2025-02-28 00:41:31 +0000

    no debug, clear answer
    0.096s

--- ALL.2015/2015.day19.2.c
+++ ALL.2015/2015.day19.2.c
@@ -6,11 +6,9 @@
 #include <map>
 #include <bits/stdc++.h>
 #include <string>
+#include <vector>
 
 #include <unistd.h>
-
-#define getchar()
-#define assert(asdf)
 using namespace std;
 
 int lenx, leny;
@@ -21,15 +19,16 @@ map <string, string> mpStore;
 map <string, string> mpB;
 char mol[1000];
 #define getchar()
+vector <string> V;
+vector <string> e_ans;
 int main(int argc, char **argv)
 {
 	lenx = 0; leny = 0;
 	//printf("%d", argc); printf("%s", argv[1]); fflush(stdout);
 	FILE * a = fopen(argv[1], "r"); 
 	printf(DAY); fflush(stdin); fflush(stdout);
-	printf("broken\n"); exit(0);
+	int fd = dup(1); close(1);
 
-	fflush(stdout); int fd = dup(1); close(1);
 	char line1[1000];
 	int readMol = 0;
 	string molString;
@@ -47,9 +46,12 @@ int main(int argc, char **argv)
 			char from[20]; char to1[20];
 			sscanf(line1, "%s => %s", from, to1);
 			string fr = from; string to = to1;
+			printf(" from %s ---- to %s\n", from, to1);
 			mp.insert({fr, to});
 			mpB.insert({to, fr});
-			printf(" from %s ---- to %s\n", from, to1);
+			if (strcmp(from, "e") == 0) {
+				e_ans.push_back(to);
+			}
 		}
 
 		if (line1[0] == '\0') {
@@ -59,69 +61,61 @@ int main(int argc, char **argv)
 		leny++;
 	}
 
+	printf("mp.size() is %d\n", (int)mp.size());
+	printf("mpB.size() is %d\n", (int)mpB.size());
+	getchar();
+	string orig = molString;
 	printf("molstring is %s\n", molString.c_str());
 	string prev;
-	int tot = 0;
+	for (auto it = mpB.rbegin(); it != mpB.rend(); it++) {
+		V.insert(V.end(), it->first);
+	}
+
+	int tot = 1;
 restart:
 	prev = molString;
-	string lens[50]; int lensPos = 0;
 	int found = -1;
-	lensPos = 0;
-	/*
-	   for (auto it = mpB.rbegin(); it != mpB.rend(); it++) {
-	   printf("%s %s\n", it->first.c_str(), it->second.c_str());
-	   }
-	   getchar();
-	   */
-	for (auto it = mpB.rbegin(); it != mpB.rend(); it++) {
-		if (it->second != "e") {
-			printf("searching for %s\n", it->first.c_str()); fflush(stdout);
-			found = -1;
-			if ((int)molString.rfind(it->first) != (int)string::npos) {
-				lens[lensPos] = it->first; lensPos++;
-			}
-			//while ((found = molString.rfind(it->first)) != (int)string::npos) {
 
-			//string molTmp = molString;
-			//molString.replace(found, it->first.length(), it->second);	
-			//mpStore.insert({molTmp, it->first});
-			//printf("molTmp %s\n", molTmp.c_str());
-			///printf("found is %d of %s\n", found, it->first.c_str()); getchar();
-			//printf("mo: %s\n", molString.c_str()); 
-			//tot++;
-			//}
+	for (int i =0; i < (int)V.size(); i++) {
+		if (mpB[V.at(i)] != "e") {
+			//printf("searching for %s\n", V.at(i).c_str()); fflush(stdout);
+			found = -1;
+			while ((found = (int)molString.rfind(V.at(i))) != (int)string::npos) {
+				molString.replace(found, V.at(i).length(), mpB[V.at(i)]);	
+				tot++;
+			}
 		}
 	}
-	int max = 0; int maxPos = 0;
-	printf("lensPos is %d\n", lensPos);
-	for (int i = 0; i < lensPos; i++) {
-		int mylen =((int)lens[i].length() - (int)mpB[lens[i]].length()); 
-		if (mylen >= max) {max = mylen; maxPos = i;}
-	}
-	printf("conv %s to %s\n", lens[maxPos].c_str(), mpB[lens[maxPos]].c_str());
-	printf("moString: %s\n", molString.c_str()); getchar();
-	found = molString.find(lens[maxPos]);
-	molString.replace(found, lens[maxPos].length(), mpB[lens[maxPos]]);	
-	/*
-	   do {
-	   if (lens[maxPos] == "") {break;}
-	   found = molString.rfind(lens[maxPos]);
-	   printf("found is %d: [%s]\n", found, lens[maxPos].c_str()); getchar();
-	   if (found != -1) {
-	   molString.replace(found, lens[maxPos].length(), mpB[lens[maxPos]]);	
-	   }
-	   } while (found != -1);
-	   */
 	if (prev != molString) {
-		printf("mo2: %s\n", molString.c_str()); 
-		printf("going to restart...\n"); getchar();
+		//printf("mo2: %s\n", molString.c_str()); 
+		//printf("going to restart...\n"); //getchar();
 		goto restart;
 	} 
-	//if (prev != molString) {
-	//	goto restart;
-	//}
 
+	//only one solution
+	printf("molString is %s\n", molString.c_str());
+	if (find(e_ans.begin(), e_ans.end(), molString) != e_ans.end()) {
+		printf("found tot: %d yatzee..\n", tot);
+		//getchar();
+		//random_shuffle(V.begin(), V.end());
+		//molString = orig;
+	} else {
+	//if (prev == molString) 
+		//printf("molString is %s\n", molString.c_str());
+		//printf("SHUFFLE...\n");
+		//printf("mo3: %s\n", molString.c_str()); 
+		//system("grep \"e =>\" i1.txt");
+		random_shuffle(V.begin(), V.end());
+		//printf("V.at(0) is %s\n", V.at(0).c_str());
+		molString = orig;
+		//tot++;
+		//printf("****tot %d\n", tot);
+		tot = 1;
+		//getchar();
+		goto restart;
+	}
+		
 	fflush(stdout); dup2(fd, 1);
-	printf("mo2: %s\n", molString.c_str()); 
-	printf("****tot %d\n", tot);
+	//printf("mo2: %s\n", molString.c_str()); 
+	printf("**ans: %d\n", tot);
 }
