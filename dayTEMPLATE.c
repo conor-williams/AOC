7bd9ee4a6be77304f243bd67c2ba94cf882faf12
conor-williams <conor.williams@gmail.com>
2025-03-05 18:50:01 +0000

    TimerSet

--- dayTEMPLATE.c
+++ dayTEMPLATE.c
@@ -8,7 +8,8 @@
 #include <signal.h>
 #include <time.h>
 #include <sys/resource.h>
-
+#include <sys/time.h>
+#include <signal.h>
 #include <unistd.h>
 ///#define assert(asf)
 #define getchar()
@@ -44,8 +45,42 @@ void sigfunc(int a) { printf("[[ %s ]]\n", "signal hand..\n"); }
 int lenx;
 int leny;
 
+void TimerStop(int signum);
+void TimerSet(int interval);
+
+void TimerSet(int interval) {
+    printf("starting timer\n");
+    struct itimerval it_val;
+
+    it_val.it_value.tv_sec = interval;
+    it_val.it_value.tv_usec = 0;
+    it_val.it_interval.tv_sec = 0;
+    it_val.it_interval.tv_usec = 0;
+
+    if (signal(SIGALRM, TimerStop) == SIG_ERR) {
+        perror("Unable to catch SIGALRM");
+        exit(1);
+    }
+    if (setitimer(ITIMER_REAL, &it_val, NULL) == -1) {
+        perror("error calling setitimer()");
+        exit(1);
+    }
+}
+
+void TimerStop(int signum) {
+
+    printf("Timer ran out! Stopping timer\n");
+        FILE *f = fopen("out.tim", "a");
+        fprintf(f, "Timer ran out! Stopping timer timestamp@%s\n", "out.tim");
+        fclose(f);
+    exit(10);
+}
+//main:::if (argc == 3) {printf("SETTING TIME TO [%d]\n", atoi(argv[2])); TimerSet(atoi(argv[2]));}
+
+
 int main(int argc, char **argv)
 {
+	TimerSet(60*14);
 	struct rlimit limit;
         getrlimit (RLIMIT_STACK, &limit);
         printf ("\nStack Limit = %ld and %ld max\n", limit.rlim_cur, limit.rlim_max);
