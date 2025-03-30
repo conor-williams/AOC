4ede055b1654388d289e5f106004ac8c06c5771f
C.P Williams <conor.williams@gmail.com>
2025-03-30 16:24:52 +0100

    clear answer, no debug < 500ms

--- ALL.2017/2017.day17.2.c
+++ ALL.2017/2017.day17.2.c
@@ -1,122 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <math.h>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <deque>
+#include <iostream>
+#include <memory>
 
-#include <unistd.h>
-
-#define getchar()
-#define assert(asdf)
+//CharlieYJH on reddit
 using namespace std;
 
-deque <long int> circle2;
-void rotateClock(int d);
-void rotateClock(int d)
+struct node {
+	int val;
+	shared_ptr<struct node> next;
+};
+
+int main(int argc, char const* argv[])
 {
-    for (int i = 0; i < d; i++) {
-        long int val = circle2.front();
-        circle2.pop_front();
-        circle2.push_back(val);
-    }
-}
-void rotate(int d);
-void rotate(int d)
-{
-    // Push first d elements
-    // from last to the beginning
-    for (int i = 0; i < d; i++) {
-        long int val = circle2.back();
-        circle2.pop_back();
-        circle2.push_front(val);
-    }
-}
+	printf("2017 Day 17 Part 2\n");
+	const int step = 354;
+	shared_ptr<struct node> head = make_shared<struct node>();
+	shared_ptr<struct node> curr = head;
+	head->val = 0;
+	head->next = head;
 
+	/*
+	for (int i = 1; i <= 2017; i++) {
 
-int tot = 0;
-#define SIZE 50
-char line1[SIZE];
+		for (int j = 0; j < step; j++)
+			curr = curr->next;
 
-#include <sys/time.h>
-#include <signal.h>
-void TimerStop(int signum);
-void TimerSet(int interval);
-
-void TimerSet(int interval) {
-    printf("starting timer\n");
-    struct itimerval it_val;
-
-    it_val.it_value.tv_sec = interval;
-    it_val.it_value.tv_usec = 0;
-    it_val.it_interval.tv_sec = 0;
-    it_val.it_interval.tv_usec = 0;
-
-    if (signal(SIGALRM, TimerStop) == SIG_ERR) {
-        perror("Unable to catch SIGALRM");
-        exit(1);
-    }
-    if (setitimer(ITIMER_REAL, &it_val, NULL) == -1) {
-        perror("error calling setitimer()");
-        exit(1);
-    }
-}
-
-int fd;
-void TimerStop(int signum) {
-	
-	fflush(stdout); dup2(fd, 1);
-    printf("Timer ran out! Stopping timer\n");
-	FILE *f = fopen("out.tim", "a");
-	fprintf(f, "Timer ran out! Stopping timer timestamp@%s\n", "out.tim");
-	fclose(f);
-    exit(10);
-}
-//main:::if (argc == 3) {printf("SETTING TIME TO [%d]\n", atoi(argv[2])); TimerSet(atoi(argv[2]));}
-int main(int argc, char **argv)
-{
-	printf("SLOW ~21minutes\n");
-	TimerSet(1200);
-        printf("%d", argc); printf("%s", argv[1]); fflush(stdout);
-        FILE * a = fopen(argv[1], "r"); printf("2017 Day17.2\n"); fflush(stdout);
-
-	fflush(stdout); fd = dup(1); close(1);
-int leny = 0;
-int spin;
-	circle2.clear();
-	circle2.push_back(0);
-while (1) 
-{
-        fgets(line1, SIZE -1, a);
-	if (feof(a)) break;
- 	line1[strlen(line1)-1] = '\0';
-// 	printf("line1 %s\n", line1);
-	
-	spin = atoi(line1);
-	leny++;
-}
-	fclose(a);
-        int newSpin;
-        for (long int i = 1; i <= 50000000; i++) {
-		if (i % 1000000 == 0) {printf("i is %ld\n", i);
-	for (auto it = circle2.begin(); it != circle2.end(); it++) { if (*it == 0) {it++; printf("** %ld\n", *it); break;}} }
-                if (i < spin) {
-                        newSpin = spin % circle2.size();
-                } else {
-                        newSpin = spin;
-                }
-                rotateClock(newSpin);
-                circle2.push_back(i);
+		shared_ptr<struct node> temp = curr->next;
+		shared_ptr<struct node> new_node = make_shared<struct node>();
+		new_node->val = i;
+		curr->next = new_node;
+		new_node->next = temp;
+		curr = new_node;
 	}
-//	for (auto it = circle2.begin(); it != circle2.end(); it++) { printf(" [%ld] ", *it); } printf("\n"); getchar();}
 
-	fflush(stdout); fflush(stdout); dup2(fd, 1);
-	printf("**ans: ");
-	for (auto it = circle2.begin(); it != circle2.end(); it++) { if (*it == 0) {it++; printf("** %ld\n", *it); break;}} 
-	printf("\n");
+	cout << curr->next->val << endl;
+	*/
+
+	int answer_pt2 = 0;
+
+	for (int i = 1, idx = 0; i <= 50000000; i++) {
+		idx = (idx + 1 + step) % i;
+		if (idx == 0) {answer_pt2 = i;}
+	}
+
+	//cout << answer_pt2 << endl;
+	printf("**ans: %d\n", answer_pt2);
 
 	return 0;
 }
