cdc9794718745c6caca813243f01c2089fdf1bbf
C.P Williams <conor.williams@gmail.com>
2025-03-30 21:00:08 +0100

    fflush needed re tee

--- ALL.2018/2018.day23.2.c
+++ ALL.2018/2018.day23.2.c
@@ -33,7 +33,7 @@ int main(int argc, char **argv)
 {
 	//printf("%d", argc); printf("@%s", argv[1]); fflush(stdout);
 	FILE * a = fopen(argv[1], "r"); printf("2018 Day23.2\n"); fflush(stdout);
-	printf("SLOW ~5minutes 50seconds\n");
+	printf("SLOW ~5minutes 50seconds\n"); fflush(stdout);
 	fd = dup(1); close(1);
 
 	leny = 0;
