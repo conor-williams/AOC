536696a39b32d81eb8a6f5453f37148a90e92f7a
C.P Williams <conor.williams@gmail.com>
2025-03-30 21:56:51 +0100

    bit slow gone

--- FIRST.sh
+++ FIRST.sh
@@ -1,5 +1,4 @@
 ### a guide to running...
-
 echo "tar of input-2015-2014.tar.gz takes ~10seconds"
 tar -xvzf input-2015-2014.tar.gz
 echo "sh specialCopy.sh takes ~1minute"
@@ -9,8 +8,9 @@ sh specialCompile.sh
 mv 2019/day25/*.exe 2019/day25/2019.day25.INTERACTIVE.exe
 echo "sh runALL.sh takes ~560minutes"
 echo "if you ps and kill the big 5:"
-echo "		(126min, 35mins, 21mins, 34mins, 4hours)"
+#echo "		(126min, 35mins, 21mins, 34mins, 4hours)"
 echo "it takes ~110minutes"
+echo "big 5 have been eliminated @ 30/03/2025 - few 3-5 mins there"
 sh runALL.sh
 
 ##creates output.2015, output.2016 etc...
@@ -18,9 +18,12 @@ echo "************REPORT:**************************"
 echo "un-tar input-2015-2014.tar.gz takes ~10seconds"
 echo "sh specialCopy.sh takes ~1minute"
 echo "sh specialCompile.sh takes ~11minutes"
-echo "sh runALL.sh takes ~560minutes"
-echo "if you ps and kill the big 5:"
-echo "		(126min, 35mins, 21mins, 34mins, 4hours)"
-echo "	it takes ~110minutes"
-echo "************REPORT:**************************"
+echo "takes ~2 hours"
+#echo "sh runALL.sh takes ~560minutes"
+#echo "if you ps and kill the big 5:"
+#echo "		(126min, 35mins, 21mins, 34mins, 4hours)"
+#echo "	it takes ~110minutes"
+#echo "************REPORT:**************************"
+
+
 
